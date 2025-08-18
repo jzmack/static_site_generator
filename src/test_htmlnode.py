daeb8b1845a423c553659545467499b1168b9634
Jake Mackin <canesfanjake1@gmail.com>
2025-08-18 14:32:44 -0400

    added ParentNode child class of HTMLNode and unit tests for it

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_initialization_defaults(self):
@@ -65,6 +65,51 @@ class TestLeafNode(unittest.TestCase):
     def test_leaf_props_to_html_empty_dict(self):
         node = LeafNode("em", "Italic", props={})
         self.assertEqual(node.props_to_html(), "")
+
+class TestParentNode(unittest.TestCase):
+    def test_valid_parent_node(self):
+        child1 = LeafNode("span", "Hello")
+        child2 = LeafNode("span", "World")
+        parent = ParentNode("div", [child1, child2])
+        self.assertEqual(parent.to_html(), "<div><span>Hello</span><span>World</span></div>")
+
+    def test_parent_node_with_props(self):
+        child = LeafNode("em", "Text")
+        parent = ParentNode("section", [child], props={"class": "highlight"})
+        self.assertEqual(parent.to_html(), '<section class="highlight"><em>Text</em></section>')
+
+    def test_missing_tag_raises_error(self):
+        child = LeafNode("p", "Content")
+        with self.assertRaises(ValueError) as context:
+            ParentNode(None, [child])
+        self.assertEqual(str(context.exception), "ParentNode requires a non-None tag.")
+
+    def test_missing_children_raises_error(self):
+        with self.assertRaises(ValueError) as context:
+            ParentNode("div", None)
+        self.assertEqual(str(context.exception), "ParentNode requires a non-None children list.")
+
+    def test_children_not_list_raises_type_error(self):
+        child = LeafNode("p", "Text")
+        with self.assertRaises(TypeError):
+            ParentNode("div", child)  # Not a list
+
+    def test_invalid_child_type_raises_type_error(self):
+        with self.assertRaises(TypeError):
+            ParentNode("div", ["not a node"])
+
+    def test_child_missing_to_html_method_raises_error(self):
+        class FakeNode:
+            pass
+        with self.assertRaises(TypeError):
+            ParentNode("div", [FakeNode()])
+
+    def test_empty_children_list_renders_valid_html(self):
+        parent = ParentNode("ul", [])
+        self.assertEqual(parent.to_html(), "<ul></ul>")
+
+if __name__ == "__main__":
+    unittest.main()
 
 
 if __name__ == "__main__":
